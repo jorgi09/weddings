c61c382370d9e8733cd2a9095f60b94cc31e114d
Michal Przybysz <michal.przybysz@ymail.com>
2013-01-11 10:03:31 +0100

    new guest form changes

--- js/application/view/guestForm.js
+++ js/application/view/guestForm.js
@@ -14,7 +14,8 @@ define([
         template: Handlebars.compile(template),
         events: {
             'blur input[type="text"]': 'update',
-            'blur input[type="number"]': 'update'
+            'blur input[type="number"]': 'update',
+            'change input[type="checkbox"]': 'checkbox'
         },
         initialize: function () {
             console.log('view', 'GuestFrom:initialize', arguments);
@@ -38,6 +39,10 @@ define([
         inputs: function () {
             console.log('view', 'GuestFrom:inputs', arguments);
             var data = {};
+            if (this.input['accompanying']) {
+                data.accompanying = this.input['accompanying'];
+            }
+            
             if (this.input['guest-first'] || this.input['guest-second']) {
                 data['guest-first'] = this.input['guest-first'] || '';
                 data['guest-second'] = this.input['guest-second'] || '';
@@ -48,10 +53,17 @@ define([
                 data['accompanying-first'] = this.input['accompanying-first'] || '';
                 data['accompanying-second'] = this.input['accompanying-second'] || '';
             }
+            
             if (this.input['childs'] && parseInt(this.input['childs'], 10) !== 0) {
                 data.childrens = parseInt(this.input['childs'], 10)
             }
+            
             return data;
+        },
+        checkbox: function (e) {
+            console.log('fdsfsdfsfsfsdfsd')
+            var $el = $(e.target);
+            this.input[$el.attr('name')] = $el.is(":checked");
         }
     });
     
